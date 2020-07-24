11ed7f3736fcac613318b1529308d5a6d8040d00
Zulkarnaen Hatala <dzulqarnaenhatala@gmail.com>
2020-07-25 08:27:10 +0900

    htktools

--- scr_herest_nophone_align/env_cyg_titml_idn.sh
+++ scr_herest_nophone_align/env_cyg_titml_idn.sh
@@ -1,4 +1,5 @@
 HTKTOOLS_DIR="../../../small_vocabulary_/HTK-3.4.1_pure/htk/HTKTools"
+HTKTOOLS_DIR="/cygdrive/f/rsync/RESEARCHS/htk/softs_tools/htk32/htk_3.4_5/HTKTools"
 #HTKTOOLS_DIR="../htk_pure/htk/HTKTools"
 titml_prefix="../../../small_vocabulary_/datas/TITML-IDN"
 #titml_prefix="../TITML-IDN"
--- scr_herest_nophone_align/full_mono_adapt.sh
+++ scr_herest_nophone_align/full_mono_adapt.sh
@@ -1,4 +1,5 @@
 sync
+#read
 source env_cyg_titml_idn.sh
 echo using prefix $titml_prefix
 #for x in `cat monophones1_full.txt`
@@ -16,37 +17,35 @@ cmd="$HTKTOOLS_DIR/HERest -C configtrain.txt -S x240_train.lst -T 1 $prune $use_
 #echo $cmd ; eval $cmd
 #echo ENTER ; read
 
-cmd="HHEd -B -H full_herest_out/04/hmmdefs  -M classes regtree.hed monophones1_full.txt"
+cmd="$HTKTOOLS_DIR/HHEd -B -H full_herest_out/04/hmmdefs  -M classes regtree.hed monophones1_full.txt"
 #echo $cmd ; eval $cmd
 #echo ENTER ; read
 
 
 mkdir -p xforms
 
-cmd="HERest -T 7 -C configtrain.txt -C config.semi -S adapt.scp $use_mlf \
+cmd="$HTKTOOLS_DIR/HERest -T 7 -C configtrain.txt -C config.semi -S adapt.scp $use_mlf \
  -u stw  -H full_herest_out/04/hmmdefs  -K xforms mllr1 -J classes  -z x.tmf  -h '*/m1*.mfc' -M tmp monophones1_full.txt"
 #echo $cmd ; eval $cmd
 #echo ENTER ; read
 
 
-cmd="HERest -T 7 -C configtrain.txt -C config.global -S adapt.scp $use_mlf \
+cmd="$HTKTOOLS_DIR/HERest -T 7 -C configtrain.txt -C config.global -S adapt.scp $use_mlf \
  -u a  -H full_herest_out/03/hmmdefs  -K xforms mllr1 -J classes  -z x.tmf  -h '*/m1*.mfc' -M tmp monophones1_full.txt"
 #echo $cmd ; eval $cmd
 #echo ENTER ; read
 
 mkdir -p hmm16
 cp x240_m11_test.lst adapt.scp
-cmd="HERest -T 1 -C configtrain.txt -C config.semi -S adapt.scp -L gen_lab -u stw \
+cmd="$HTKTOOLS_DIR/HERest -T 1 -C configtrain.txt -C config.semi -S adapt.scp -L gen_lab -u stw \
 -H full_herest_out/04/hmmdefs   -h '*/m11*.mfc' -J classes \
 -K HMM16 -M hmm16 monophones1_full.txt"
-echo $cmd ; eval $cmd
-echo ENTER ; read
+echo $cmd ; eval $cmd ;echo ENTER ; read
 
 
 mkdir -p hmm17
-cmd="HERest -T 1 -C configtrain.txt  -S adapt.scp -L gen_lab -u tmvw \
+cmd="$HTKTOOLS_DIR/HERest -T 1 -C configtrain.txt  -S adapt.scp -L gen_lab -u tmvw \
 -H hmm16/hmmdefs   -h '*/m11*.mfc' -J classes -J hmm16 \
  -M hmm17 monophones1_full.txt"
-echo $cmd ; eval $cmd
-echo ENTER ; read
+#echo $cmd ; eval $cmd ; echo ENTER ; read
 
